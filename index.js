6fb57075709079ceb0321fe3190d13ab666738e7
immanuel3333 <immanuel.siburian80@gmail.com>
2021-12-10 09:05:09 +0700

    setup express

---
+++ index.js
@@ -0,0 +1,14 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(port, () => {
+  console.log("server is listening on port", port);
+});
