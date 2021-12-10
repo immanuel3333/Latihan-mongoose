114ba96f9ac1f19b6355cfa7abb5e95c8759e5e5
immanuel3333 <immanuel.siburian80@gmail.com>
2021-12-10 10:08:32 +0700

    setup mongodb connection

--- index.js
+++ index.js
@@ -4,12 +4,33 @@ dotenv.config();
 const express = require("express");
 
 const routes = require("./routes");
+const db = require("./helpers/db");
 
-const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(routes);
+const uri = process.env.MONGO_URI;
 
-app.listen(port, () => {
-  console.log("server is listening on port", port);
-});
+// Kalau ada pass
+// const dbOptions = {
+//   user: process.env.MONGO_USER,
+//   pass: process.env.MONGO_PASS,
+// };
+// db.openDBConnection(uri, dbOptions);
+
+async function main() {
+  try {
+    //mastikan database connect
+    await db.openDBConnection(uri, {});
+
+    const app = express();
+    app.use(routes);
+
+    app.listen(port, () => {
+      console.log("server is listening on port", port);
+    });
+  } catch (error) {
+    console.log("main:", error);
+  }
+}
+
+main(); // menjalankan main
