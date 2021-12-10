743b2119f7009dbef7b15843e2521f4f05aabe7c
immanuel3333 <immanuel.siburian80@gmail.com>
2021-12-10 09:21:13 +0700

    setup ping routes

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ dotenv.config();
 
 const express = require("express");
 
+const routes = require("./routes");
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(routes);
 
 app.listen(port, () => {
   console.log("server is listening on port", port);
